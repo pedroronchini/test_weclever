956e2207bc48e1208ef3cb5226068bfb1f40de5a
Pedro <pe-ronchini@hotmail.com>
2021-10-22 23:53:19 -0300

    create: page Table

---
+++ test_weclever/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <Table />
+</template>
+
+<script>
+import Table from './components/Table.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Table
+  }
+}
+</script>
+
+<style>
+body {
+  background: #000000;
+}
+</style>
---
+++ test_weclever/src/components/Table.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="hello">
+    <table class="table">
+      <tr>
+        <th>Name</th>
+        <th>Height</th>
+        <th>Mass</th>
+        <th>Birth year</th>
+        <th>Gender</th>
+      </tr>
+      <tr v-for="list in list " :key="list">
+        <td>{{ list.name }}</td>
+        <td>{{ list.height }}</td>
+        <td>{{ list.mass }}</td>
+        <td>{{ list.birth_year }}</td>
+        <td>{{ list.gender }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Table',
+
+  data () {
+    return {
+      list: []
+    }
+  },
+
+  mounted () {
+    axios.get('https://swapi.dev/api/people/')
+        .then(response => {
+          const jsonobjecta = JSON.stringify(response.data)
+          const object = JSON.parse(jsonobjecta)
+        console.log(object.results[1])
+          this.list = object.results[0]
+    })
+    
+
+  }
+}
+</script>
+
+<style scoped>
+table {
+  width:100%;
+}
+
+table, th, td {
+  border-top: 2px solid #FFFFFF;
+  border-collapse: collapse;
+  color: #FFe81F;
+}
+
+th, td {
+  padding: 15px;
+  text-align: left;
+}
+
+#t01 tr:nth-child(even) {
+  background-color: #eee;
+}
+
+#t01 tr:nth-child(odd) {
+ background-color: #FFFFFF;
+}
+
+#t01 th {
+  background-color: #000000;
+  color: #FFFFFF;
+}
+</style>
